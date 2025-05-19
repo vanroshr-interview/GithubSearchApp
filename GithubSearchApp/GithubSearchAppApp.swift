1d7bd47a754b17ccab5655c38bae082573ea071a
Pratish Shrestha <pratishshr@gmail.com>
2025-05-19 19:35:14 +0545

    Initial Commit

---
+++ GithubSearchApp/GithubSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchAppApp.swift
+//  GithubSearchApp
+//
+//  Created by Pratish Shrestha on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
